92cdc40e43aee5121bf5c3718ee8f27ec2141978
Dean Willis <deanwillis@outlook.com>
2025-10-26 16:43:51 -0400

    test

--- website/main.py
+++ website/main.py
@@ -1,38 +1,6 @@
-from flask import Flask, render_template, request 
 from website import create_app
-import json
-import os 
-#test
-
-
 
 app = create_app()
-
-
-if __name__ 
-
-
-@app.route('/',methods =['GET','POST'])
-def home():
-
-    if os.path.exists("users_info.json"):
-        with open('users_info.json',"r") as f:
-            try:
-                users = json.load(f)
-            except json.JSONDecodeError:
-                users = []
-    else:
-        users = []        
-
-    if request.method == "POST":
-        name = request.form['name']
-        users.append({'name':name})
-
-        with open ('users_info.json','w')as f:
-            json.dump(users, f, indent=4)
-
-    return render_template("form.html", users =users)
-    
 
 if __name__ == '__main__':
     app.run(debug=True)

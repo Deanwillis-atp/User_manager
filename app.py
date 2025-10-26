ebc225d6380ee9a437e6593839f783f225481ebd
Deanwillis-atp <deanwillis@outlook.com>
2025-10-26 12:19:56 -0400

    #update

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from flask import Flask, render_template, request 
-#test
+import json
+import os 
+
+
 app = Flask(__name__)
 
 @app.route('/',methods =['GET','POST'])
 def home():
+
+    if os.path.exists("users_info.json"):
+        with open('users_info.json',"r") as f:
+            try:
+                users = json.load(f)
+            except json.JSONDecodeError:
+                users = []
+    else:
+        users = []        
+
     if request.method == "POST":
         name = request.form['name']
-        
+        users.append({'name':name})
 
-    return render_template("form.html")
+        with open ('users_info.json','w')as f:
+            json.dump(users, f, indent=4)
+
+    return render_template("form.html", users =users)
     
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
-
